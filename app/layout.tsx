756409b9798115b9f5e893bb0cf3a5a1a1e9f14d
Gabriela Sombra <gsombra@showcasepro.com.br>
2024-05-13 10:26:49 -0300

    fixes sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,14 +18,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={'${inter.className} bg-neutral-900 flex items-start justify-between'}>
-        <Sidebar />
-        <main className="flex w-full h-full mx-2">
+      <body className={'${inter.className}  bg-neutral-900 flex items-start justify-between'}>
+        <div className="fixed top-0 left-0 h-full">
+          <Sidebar />
+        </div>
+        <main className="flex w-full h-full mx-2 ml-[368px]">
           <div className="w-2/3">
             <Header />
             {children}
           </div>
-          <div className="w-1/3 ">
+          <div className="w-1/3">
             <RightPanel />
           </div>
         </main>
@@ -33,3 +35,5 @@ export default function RootLayout({
     </html>
   );
 }
+
+
