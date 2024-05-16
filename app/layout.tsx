3756bda7c162fe01f8b1fded3b75f4bf449f637c
Gabriela Sombra <gsombra@showcasepro.com.br>
2024-05-16 16:37:24 -0300

    scrollbar fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={'${inter.className}  bg-neutral-900 flex items-start justify-between'}>
+      <body className={'${inter.className}  bg-neutral-900 flex items-start justify-between  '}>
         <div className="fixed top-0 left-0 h-full">
           <Sidebar />
         </div>
